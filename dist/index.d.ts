e3a69697d95492866adff96404ca0f4e3a2c3bf0
Adrien Martineau <contact@adrien-martineau.fr>
2023-09-06 17:21:20 +0200

    3.0.1

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,5 @@ export { UrlBuilder } from './url-builder';
 export { UrlUtils } from './url.utils';
 export { UrlConstants } from './url.constants';
 export { IFile } from './types';
+export { PathParams } from './maps/path-params';
+export { QueryParams } from './maps/query-params';
--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UrlConstants = exports.UrlUtils = exports.UrlBuilder = void 0;
+exports.QueryParams = exports.PathParams = exports.UrlConstants = exports.UrlUtils = exports.UrlBuilder = void 0;
 var url_builder_1 = require("./url-builder");
 Object.defineProperty(exports, "UrlBuilder", { enumerable: true, get: function () { return url_builder_1.UrlBuilder; } });
 var url_utils_1 = require("./url.utils");
 Object.defineProperty(exports, "UrlUtils", { enumerable: true, get: function () { return url_utils_1.UrlUtils; } });
 var url_constants_1 = require("./url.constants");
 Object.defineProperty(exports, "UrlConstants", { enumerable: true, get: function () { return url_constants_1.UrlConstants; } });
+var path_params_1 = require("./maps/path-params");
+Object.defineProperty(exports, "PathParams", { enumerable: true, get: function () { return path_params_1.PathParams; } });
+var query_params_1 = require("./maps/query-params");
+Object.defineProperty(exports, "QueryParams", { enumerable: true, get: function () { return query_params_1.QueryParams; } });
